940277558e99db4236e3a10339c0fdedb47871cf
daniel-arevalob <oneprotocol@hotmail.com>
2026-04-11 20:27:22 -0500

    test

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    v5 — 2026-04-11
 ═══════════════════════════════════════════════ */
 
-const CACHE_V = 'op-web-v11'; // ← increment on each deploy
+const CACHE_V = 'op-web-v12'; // ← increment on each deploy
 
 const PRECACHE = [
   '/',
